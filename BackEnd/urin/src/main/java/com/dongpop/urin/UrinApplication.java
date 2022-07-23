db9af76d0d8883706df13f1abdd03b89ffa09e0a
김현교 <zkdlajt96@naver.com>
2022-07-23 22:20:19 +0900

    first commit

---
+++ BackEnd/urin/src/main/java/com/dongpop/urin/UrinApplication.java
@@ -0,0 +1,13 @@
+package com.dongpop.urin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrinApplication.class, args);
+	}
+
+}
---
+++ BackEnd/urin/src/test/java/com/dongpop/urin/UrinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongpop.urin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
