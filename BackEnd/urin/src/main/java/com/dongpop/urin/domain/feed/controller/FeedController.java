e408a0fafdb634468962ce5249f77898bb2f3c6a
김현교 <zkdlajt96@naver.com>
2022-08-01 18:46:42 +0900

    :hammar: chore : Merge Request 수정 #S07P12A504-201
    설정파일, 테스트 파일 제거

--- BackEnd/urin/src/main/java/com/dongpop/urin/domain/feed/controller/FeedController.java
+++ BackEnd/urin/src/main/java/com/dongpop/urin/domain/feed/controller/FeedController.java
@@ -10,7 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
