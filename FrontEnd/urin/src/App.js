608286d2d04cae6fdea2cdb66ad5d17cdf128bd0
김신철 <zzangsin2002@gmail.com>
2022-08-04 00:29:12 +0900

    tmp

--- FrontEnd/urin/src/App.js
+++ FrontEnd/urin/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import LandingPage from "./pages/LandingPage";
 import MainPage from "./pages/MainPage";
@@ -15,12 +16,28 @@ import Logout from "./pages/Logout";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import PublicRoute from "./components/PublicRoute/PublicRoute";
 import NavComponent from "./components/Navbar";
+import { checkValidation } from "./store/checkValidationSlice";
 
 const App = () => {
   const privateAccess = !!localStorage.getItem("accessToken");
+  const memberId = localStorage.getItem("accessToken");
   const publicAccess = !privateAccess;
 
-  useEffect(() => {});
+  const dispatch = useDispatch();
+  const validation = useSelector((state) => state.validation);
+
+  // useParams는 react-router와 관련
+  // 현재 위치가 /study/:studyId이기 때문에
+  // : 뒤에 있는 studyId를 가져올 수 있음
+
+  // useEffect의 첫번째 인자: 컴퍼넌트가 mounted, updated, unmounted될 때 수행할 함수
+  // 최초 진입시 컴퍼넌트의 study는 initialState상태(정보가 없는 상태)이므로 dispatch로 데이터를 불러와야 함
+  // useEffect의 두번째 인자:
+  // 없는 경우, 빈 배열, 배열 안에 의존값 => https://react.vlpt.us/basic/16-useEffect.html
+  useEffect(() => {
+    dispatch(checkValidation(memberId));
+  }, []);
+
   // const [studyId, setStudyId] = useState(1);
   // const [meetingId, setMeetingId] = useState(1);
 
---
+++ FrontEnd/urin/src/store/checkValidationSlice.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  createAsyncThunk,
+  createSlice,
+  isRejectedWithValue,
+} from "@reduxjs/toolkit";
+import axiosInstance from "../api";
+
+export const checkValidation = createAsyncThunk(
+  "CHECK_VALIDATION",
+  async (memberId) => {
+    try {
+      console.log("로그인 여부 가져오는 중");
+      console.log(memberId);
+      const response = await axiosInstance.get(`member/${memberId}/validation`);
+      return response.data;
+    } catch (err) {
+      console.log("=================================================", err);
+      return isRejectedWithValue(err.response.data);
+    }
+  }
+);
+
+const checkValidationSlice = createSlice({
+  // state에 들어가는 이름
+  // 각 컴퍼넌트에서 state.study 이런 식으로 부를 때 사용
+  name: "validation",
+  // 초기 상태, 백으로부터 데이터를 불러오는데 혹시라도 실패한다면 이 데이터가 보이기 때문에
+  // 빈 객체 보다는 적당히 형태를 잡아두는게 좋음
+  initialState: {
+    validation: false,
+  },
+  reducers: {},
+
+  extraReducers: {
+    [checkValidation.fulfilled]: (state, { payload }) => payload,
+  },
+});
+
+export default checkValidationSlice.reducer;
