991261da9ebc7b17d1545f8620eafa422ad495af
안웅진 <ad1230023@gmail.com>
2022-08-03 18:57:36 +0900

    :sparkles: feat validation 기능 생성
    각 입력별 유효성 검증 오류 출력

--- BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/dto/ParticipantDto.java
+++ BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/dto/ParticipantDto.java
@@ -3,11 +3,19 @@ package com.dongpop.urin.domain.participant.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Builder
 @Data
 public class ParticipantDto {
+    @Positive
     private int id;
+    @Positive
     private int memberId;
+    @NotBlank
     private String nickname;
+    @NotNull
     private Boolean isLeader;
 }
