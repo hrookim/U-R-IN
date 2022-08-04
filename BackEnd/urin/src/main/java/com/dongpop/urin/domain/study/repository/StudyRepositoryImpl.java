1edd18155f032f9f358fefd7b8b9b893f5bc1723
김현교 <zkdlajt96@naver.com>
2022-08-05 00:05:45 +0900

    :sparkles: feat : 스터디 리스트 API에 모집 중 필터 추가
    - QueryDSL 의존성 추가.
    - QueryDSL을 통한 동적 쿼리.

---
+++ BackEnd/urin/src/main/java/com/dongpop/urin/domain/study/repository/StudyRepositoryImpl.java
@@ -0,0 +1,62 @@
+package com.dongpop.urin.domain.study.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.dongpop.urin.domain.study.repository.QStudy.study;
+import static com.dongpop.urin.domain.study.repository.StudyStatus.RECRUITING;
+import static com.dongpop.urin.domain.study.repository.StudyStatus.TERMINATED;
+
+public class StudyRepositoryImpl implements StudyRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public StudyRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Page<Study> findStudyList(String keyword, boolean isRecruiting, Pageable pageable) {
+        List<Study> contents = queryFactory
+                .selectFrom(study)
+                .where(keywordLike(keyword),
+                        statusIsRecruiting(isRecruiting),
+                        statusNotInTerminated())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(study.count())
+                .from(study)
+                .where(keywordLike(keyword),
+                        statusIsRecruiting(isRecruiting),
+                        statusNotInTerminated());
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression keywordLike(String keyword) {
+        System.out.println("\n===== 쿼리 확인 =====");
+        System.out.println("keyword : " + keyword);
+        System.out.println("Boolean : " + StringUtils.hasText(keyword));
+        System.out.println("====================\n");
+        return StringUtils.hasText(keyword) ? study.title.contains(keyword) : null;
+    }
+
+    private BooleanExpression statusIsRecruiting(boolean isRecruiting) {
+        return isRecruiting ? study.status.eq(RECRUITING) : null;
+    }
+
+    private BooleanExpression statusNotInTerminated() {
+        return study.status.notIn(TERMINATED);
+    }
+}
