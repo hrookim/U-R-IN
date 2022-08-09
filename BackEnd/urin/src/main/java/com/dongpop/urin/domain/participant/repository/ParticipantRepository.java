6745ce29c86ab348a00843146b996bfc3ea96e8b
김현교 <zkdlajt96@naver.com>
2022-08-09 13:42:15 +0900

    :bug: fix : StudyService 컴파일 오류 수정
    - StudyService 컴파일 오류 수정.

--- BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/repository/ParticipantRepository.java
+++ BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/repository/ParticipantRepository.java
@@ -1,9 +1,11 @@
 package com.dongpop.urin.domain.participant.repository;
 
+import com.dongpop.urin.domain.member.entity.Member;
 import com.dongpop.urin.domain.participant.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
@@ -11,4 +13,6 @@ public interface ParticipantRepository extends JpaRepository<Participant, Intege
             " where p.study.id = :studyId" +
             " AND p.isLeader IS TRUE")
     Optional<Participant> findLeader(int studyId);
+
+    List<Participant> findAllByMember(Member member);
 }
