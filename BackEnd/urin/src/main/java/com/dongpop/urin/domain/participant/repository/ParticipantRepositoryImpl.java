be3bf9e375ec6eedcc2114f7922daa646203049c
김현교 <zkdlajt96@naver.com>
2022-08-10 13:41:57 +0900

    :recycle: refactor : Participant 삭제 요구사항 변경에 따른 StudyService 로직 변경 #S07P12A504-368
    - Participant 삭제 컬럼에 맞게 로직 수정.
    - MyStudy요청 쿼리 수정.

--- BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/repository/ParticipantRepositoryImpl.java
+++ BackEnd/urin/src/main/java/com/dongpop/urin/domain/participant/repository/ParticipantRepositoryImpl.java
@@ -2,14 +2,11 @@ package com.dongpop.urin.domain.participant.repository;
 
 import com.dongpop.urin.domain.member.entity.Member;
 import com.dongpop.urin.domain.participant.entity.Participant;
-import com.dongpop.urin.domain.participant.entity.QParticipant;
-import com.dongpop.urin.domain.study.dto.request.StudyMyDto;
-import com.dongpop.urin.domain.study.entity.StudyStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.dongpop.urin.domain.participant.entity.QParticipant.*;
 import static com.dongpop.urin.domain.study.entity.StudyStatus.*;
@@ -25,7 +22,7 @@ public class ParticipantRepositoryImpl implements ParticipantRepositoryCustom {
     @Override
     public List<Participant> findMyCurrentStudyParticipants(Member member) {
         return queryFactory.selectFrom(participant)
-                .where(participant.study.status.notIn(TERMINATED), participant.member.eq(member))
+                .where(participant.member.eq(member), isPastStudy().not())
                 .orderBy(participant.study.id.asc())
                 .fetch();
     }
@@ -33,8 +30,12 @@ public class ParticipantRepositoryImpl implements ParticipantRepositoryCustom {
     @Override
     public List<Participant> findMyTerminatedStudyParticipants(Member member) {
         return queryFactory.selectFrom(participant)
-                .where(participant.study.status.eq(TERMINATED), participant.member.eq(member))
+                .where(participant.member.eq(member), isPastStudy())
                 .orderBy(participant.study.updatedAt.asc())
                 .fetch();
+    }
+
+    private BooleanExpression isPastStudy() {
+        return participant.study.status.eq(TERMINATED).or(participant.withdrawal.isTrue());
     }
 }
