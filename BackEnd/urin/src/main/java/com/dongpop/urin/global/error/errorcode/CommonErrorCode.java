89ffdf0cb0f513e7abe34579280aa4fa566e22a1
김현교 <zkdlajt96@naver.com>
2022-08-17 11:19:19 +0900

    :bug: fix : 합격자 API 버그 수정 #S07P12A504-380
    - 합격자 API 버그 수정.

--- BackEnd/urin/src/main/java/com/dongpop/urin/global/error/errorcode/CommonErrorCode.java
+++ BackEnd/urin/src/main/java/com/dongpop/urin/global/error/errorcode/CommonErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
+    DO_NOT_HAVE_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "You don't have an authentication."),
     DO_NOT_HAVE_AUTHORIZATION(HttpStatus.FORBIDDEN, "You don't have an authorization."),
     NO_SUCH_ELEMENTS(HttpStatus.NOT_FOUND, "Don't exist such resource.")
     ;
